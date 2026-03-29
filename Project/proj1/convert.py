86fb95d8e642585bbe8b9104eba1ba9e32c247fd
ruossipha <ilcpm@outlook.com>
2026-03-29 17:28:32 +0800

    初始化project01，初始化转换图片的convert.py

---
+++ Project/proj1/convert.py
@@ -0,0 +1,17 @@
+from PIL import Image
+
+image_path = "./inp.jpg"
+output_path = "./studentOutputs/out.ppm"
+
+img = Image.open(image_path).convert('RGB')
+height, width = img.size
+
+pixels = map(lambda t: f"{t[0]:>4}{t[1]:>4}{t[2]:>4}", img.getdata())
+# print(next(pixels))
+
+with open(output_path, "w", encoding="utf-8") as f:
+    print("P3", file=f)
+    print(height, width, file=f)
+    print("255", file=f)
+    for row in range(height):
+        print("    ".join([next(pixels) for col in range(width)]), file=f)
